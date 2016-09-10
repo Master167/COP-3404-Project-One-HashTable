d6a1cd4f82a699b398acef7b9d043c036a9944c4
Michael Frederick <master16789@gmail.com>
2016-09-10 15:22:41 -0400

    Built hashFunction, and getNext
    Not sure I need the iterator.

--- src/project/one/HashTable.java
+++ src/project/one/HashTable.java
@@ -1,5 +1,8 @@
 package project.one;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 /**
@@ -15,19 +18,74 @@ public class HashTable {
      * @param args the command line arguments with the first being the filename
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            if (args[0] != null) {
+                HashArray hasher = new HashArray(args[0]);
+            }
+        }
+        catch (Exception e) {
+            System.out.printf(e.toString());
+        }
+        return;
     }
     
 }
 
 class HashArray { 
     
-    public DataItem[] data;
+    private DataItem[] data;
+    private int size;
     
-    public HashArray() {
+    public HashArray(String fileName) {
+        this.size = 1024;
         
+        try {
+            File file = new File(fileName);
+            this.data = createArray(file);
+        }
+        catch (Exception e) {
+            System.out.printf(e.toString());
+        }
     }
     
+    private DataItem[] createArray(File file) {
+        DataItem[] temp = new DataItem[this.size];
+
+        try {
+            Scanner fileReader = new Scanner(file);
+            DataItem tempData;
+            int i = 0;
+            while (fileReader.hasNextLine()) {
+                String[] string = fileReader.nextLine().split(" ");
+                if (string.length == 2) {
+                    tempData = new DataItem(string[0], Integer.parseInt(string[1]));
+                }
+                else {
+                    tempData = new DataItem(string[0]);
+                }
+                
+                temp[hashFunction(tempData.getLabel(), this.size)] = tempData;
+            }
+        }
+        catch (FileNotFoundException ex) {
+            System.out.printf(file.getName() + " was not found.%n");
+        }
+      
+        return temp;
+    }
+    
+    private int hashFunction(String str, int maxSize) {
+        int value;
+        char[] charArray = str.toCharArray();
+        int temp;
+        value = charArray[0];
+        for (int i = 1; i < charArray.length; i++) {
+            temp = charArray[i];
+            value = (value * 26 + temp) % maxSize;
+        }
+        System.out.printf("%d was calculated%n", value);
+        return value;
+    }
 }
 
 class ArrayIterator {
@@ -48,7 +106,18 @@ class ArrayIterator {
         this.lastIndex = array.length;
     }
     
-    // Create a getNext() method, 
+    public DataItem getNext() {
+        DataItem item;
+        if (currentIndex + 1 != lastIndex) {
+            item = data[++currentIndex];
+        }
+        else {
+            item = data[0];
+            currentIndex = 0;
+        }
+        
+        return item;
+    }
 }
 
 class DataItem {
